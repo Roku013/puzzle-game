a951b271c3b5ddd44085993959be845c92bf23f6
Roku013 <roku.mail@gmail.com>
2022-02-21 15:16:14 +0100

    added herbs and finish line

--- main.js
+++ main.js
@@ -2,27 +2,34 @@
 class GridSystem {
   constructor(matrix, playerX, playerY) {
     this.matrix = matrix;
-    this.uiContext = this.#getContext(420, 580, '#000');
-    this.outlineContext = this.#getContext(0, 0, '#444');
-    this.topContext = this.#getContext(0, 0, '#111', true);
+    this.uiContext = this.#getContext(420, 580, '#000'); //UI part of the screen
+    this.outlineContext = this.#getContext(0, 0, '#000'); //Grid
+    this.topContext = this.#getContext(0, 0, '#111', true); //Player and enviroment
     this.cellSize = 40;
     this.padding = 2;
     this.player = { x: playerX, y: playerY, color: 'orange' };
     this.matrix[playerY][playerX] = 2;
-
-    //life tracking
-    this.life = 10;
+    this.health = 10;
 
     //button trigger
     document.addEventListener('keydown', this.#movePlayer);
   }
 
   //check if the move is valid by checking the number of the tile on which the player is moving into
-  #isValidMove(x, y) {
+  #isValidMove(y, x) {
     if (
       this.matrix[this.player.y + y][this.player.x + x] === 0 ||
-      this.matrix[this.player.y + y][this.player.x + x] === 3
+      this.matrix[this.player.y + y][this.player.x + x] === 3 ||
+      this.matrix[this.player.y + y][this.player.x + x] === 9
     ) {
+      return true;
+    }
+    return false;
+  }
+
+  //check if the player moves into a healing herb and if so, change Health
+  #isAHerb(y, x) {
+    if (this.matrix[this.player.y + y][this.player.x + x] === 3) {
       return true;
     }
     return false;
@@ -33,20 +40,98 @@ class GridSystem {
     this.matrix[y][x] = val;
   }
 
-  // player movement. If the move is valid, chenge the number of the last tile to 0(empty), and change the number of the tile the player is now on to 2(player)
-  #movePlayer = ({ keyCode }) => {
-    if (keyCode === 37) {
+  // player movement.
+  // If the move is valid, chenge the number of the last tile to 0(empty), and change the number of the tile the player is now on to 2(player)
+  #movePlayer = () => {
+    switch (event.key) {
+      case 'ArrowLeft':
+        event.preventDefault();
+        if (this.#isValidMove(0, -1) && this.#isAHerb(0, -1)) {
+          this.#updateMatrix(this.player.y, this.player.x, 0);
+          this.#updateMatrix(this.player.y, this.player.x - 1, 2);
+          this.health = 3;
+          this.player.x--;
+          this.render();
+        } else if (this.#isValidMove(0, -1)) {
+          this.#updateMatrix(this.player.y, this.player.x, 0);
+          this.#updateMatrix(this.player.y, this.player.x - 1, 2);
+          this.health--;
+          this.player.x--;
+          this.render();
+        }
+        break;
+      case 'ArrowRight':
+        event.preventDefault();
+        if (this.#isValidMove(0, 1) && this.#isAHerb(0, 1)) {
+          this.#updateMatrix(this.player.y, this.player.x, 0);
+          this.#updateMatrix(this.player.y, this.player.x + 1, 2);
+          this.health = 3;
+          this.player.x++;
+          this.render();
+        } else if (this.#isValidMove(0, 1)) {
+          this.#updateMatrix(this.player.y, this.player.x, 0);
+          this.#updateMatrix(this.player.y, this.player.x + 1, 2);
+          this.health--;
+          this.player.x++;
+          this.render();
+        }
+        break;
+      case 'ArrowUp':
+        event.preventDefault();
+        if (this.#isValidMove(-1, 0) && this.#isAHerb(-1, 0)) {
+          this.#updateMatrix(this.player.y, this.player.x, 0);
+          this.#updateMatrix(this.player.y - 1, this.player.x, 2);
+          this.health = 3;
+          this.player.y--;
+          this.render();
+        } else if (this.#isValidMove(-1, 0)) {
+          this.#updateMatrix(this.player.y, this.player.x, 0);
+          this.#updateMatrix(this.player.y - 1, this.player.x, 2);
+          this.health--;
+          this.player.y--;
+          this.render();
+        }
+        break;
+      case 'ArrowDown':
+        event.preventDefault();
+        if (this.#isValidMove(1, 0) && this.#isAHerb(1, 0)) {
+          this.#updateMatrix(this.player.y, this.player.x, 0);
+          this.#updateMatrix(this.player.y + 1, this.player.x, 2);
+          this.health = 3;
+          this.player.y++;
+          this.render();
+        } else if (this.#isValidMove(1, 0)) {
+          this.#updateMatrix(this.player.y, this.player.x, 0);
+          this.#updateMatrix(this.player.y + 1, this.player.x, 2);
+          this.health--;
+          this.player.y++;
+          this.render();
+        }
+        break;
+    }
+    /*if (keyCode === 37) {
       event.preventDefault();
-      if (this.#isValidMove(-1, 0)) {
+      if (this.#isValidMove(0, -1) && this.#isAHerb(0, -1)) {
         this.#updateMatrix(this.player.y, this.player.x, 0);
         this.#updateMatrix(this.player.y, this.player.x - 1, 2);
-        this.life--;
+        this.health = 3;
+        this.player.x--;
+        this.render();
+      } else if (this.#isValidMove(0, -1)) {
+        this.#updateMatrix(this.player.y, this.player.x, 0);
+        this.#updateMatrix(this.player.y, this.player.x - 1, 2);
         this.player.x--;
         this.render();
       }
     } else if (keyCode === 39) {
       event.preventDefault();
-      if (this.#isValidMove(1, 0)) {
+      if (this.#isValidMove(0, 1) && this.#isAHerb(0, 1)) {
+        this.#updateMatrix(this.player.y, this.player.x, 0);
+        this.#updateMatrix(this.player.y, this.player.x + 1, 2);
+        this.health = 3;
+        this.player.x++;
+        this.render();
+      } else if (this.#isValidMove(0, 1)) {
         this.#updateMatrix(this.player.y, this.player.x, 0);
         this.#updateMatrix(this.player.y, this.player.x + 1, 2);
         this.player.x++;
@@ -54,7 +139,13 @@ class GridSystem {
       }
     } else if (keyCode === 38) {
       event.preventDefault();
-      if (this.#isValidMove(0, -1)) {
+      if (this.#isValidMove(-1, 0) && this.#isAHerb(-1, 0)) {
+        this.#updateMatrix(this.player.y, this.player.x, 0);
+        this.#updateMatrix(this.player.y - 1, this.player.x, 2);
+        this.health = 3;
+        this.player.y--;
+        this.render();
+      } else if (this.#isValidMove(-1, 0)) {
         this.#updateMatrix(this.player.y, this.player.x, 0);
         this.#updateMatrix(this.player.y - 1, this.player.x, 2);
         this.player.y--;
@@ -62,13 +153,19 @@ class GridSystem {
       }
     } else if (keyCode === 40) {
       event.preventDefault();
-      if (this.#isValidMove(0, 1)) {
+      if (this.#isValidMove(1, 0) && this.#isAHerb(1, 0)) {
+        this.#updateMatrix(this.player.y, this.player.x, 0);
+        this.#updateMatrix(this.player.y + 1, this.player.x, 2);
+        this.health = 3;
+        this.player.y++;
+        this.render();
+      } else if (this.#isValidMove(1, 0)) {
         this.#updateMatrix(this.player.y, this.player.x, 0);
         this.#updateMatrix(this.player.y + 1, this.player.x, 2);
         this.player.y++;
         this.render();
       }
-    }
+    }*/
   };
 
   //center the window
@@ -119,12 +216,19 @@ class GridSystem {
         const cellVal = this.matrix[row][col];
         let color = '#111';
 
-        if (cellVal === 1) {
-          color = '#4488FF';
-        } else if (cellVal === 2) {
-          color = this.player.color;
-        } else if (cellVal === 3) {
-          color = 'green';
+        switch (cellVal) {
+          case 1: //outer walls
+            color = '#4488FF';
+            break;
+          case 2: //player
+            color = this.player.color;
+            break;
+          case 3: //herb
+            color = 'green';
+            break;
+          case 9: //finish
+            color = 'red';
+            break;
         }
 
         this.outlineContext.fillStyle = color;
@@ -137,23 +241,24 @@ class GridSystem {
       }
     }
 
+    //Health display
     this.uiContext.font = '20px monospace';
     this.uiContext.fillStyle = 'white';
-    this.uiContext.fillText('LIFE:' + this.life, 20, 30);
+    this.uiContext.fillText('HEALTH:' + this.health, 20, 30);
   }
 }
 
 //level example for testing
 const gridMatrix = [
-  [1, 1, 1, 1, 1, 1, 1],
-  [1, 0, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 3, 0, 1],
-  [1, 0, 1, 1, 1, 0, 1],
-  [1, 0, 0, 0, 1, 0, 1],
-  [1, 0, 1, 1, 1, 0, 1],
-  [1, 0, 0, 0, 0, 0, 1],
-  [1, 1, 1, 1, 1, 1, 1]
+  [1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 3, 0, 1, 1],
+  [1, 0, 1, 1, 1, 0, 1, 1],
+  [1, 0, 0, 0, 1, 0, 1, 1],
+  [1, 0, 1, 1, 1, 0, 1, 1],
+  [1, 0, 0, 0, 0, 0, 0, 1],
+  [1, 1, 1, 1, 1, 9, 1, 1]
 ];
 
 const gridSystem = new GridSystem(gridMatrix, 1, 1);
